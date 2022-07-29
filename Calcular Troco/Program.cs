015a9d6ea3f3e9add33503ceaa5938e0faf39938
Joao Roas <jr.roas@gmail.com>
2022-07-29 14:18:37 -0300

    Add exercicios

---
+++ Calcular Troco/Program.cs
@@ -0,0 +1,30 @@
+﻿
+static void CalcularTroco()
+{
+    /* João é cobrador de ônibus, ao atender um passageiro.
+     João recebe um valor em dinheiro.
+     Faça um programa que solicite o dado de entrada, menos o 
+     dado de saída, mostrando o troco que ele precisa dar ao passageiro.
+    */
+
+    var valorPassagem = 10;
+
+    System.Console.WriteLine("Digite o valor recebido:");
+    var valorPago = Convert.ToDouble(Console.ReadLine());
+
+
+    if (valorPago < valorPassagem)
+    {
+        System.Console.WriteLine("A quantia não está completa:");
+        System.Console.WriteLine($"O valor da passagem é: {valorPassagem}");
+        return;
+    }
+    else
+    {
+        var troco = valorPago - valorPassagem;
+
+        System.Console.WriteLine($"O troco deve ser de: {troco}");
+        System.Console.WriteLine("Pressione enter para finalizar:");
+        Console.ReadLine();
+    }
+}
---
+++ Mostrar número maior/Program.cs
@@ -0,0 +1,33 @@
+﻿static void MostrarNumeroMaior()
+{
+    /* Dado que eu leia 3 números, após a leitura desses números mostrar
+    na tela qual o numero maior.
+    */
+
+    Console.WriteLine("Digite o primeiro número:");
+    var num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número:");
+    var num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o terceiro número:");
+    var num3 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > num2 && num1 > num3)
+    {
+        System.Console.WriteLine($"O número maior é: {num1}");
+    }
+    else if (num2 > num1 && num2 > num3)
+    {
+        System.Console.WriteLine($"O número maior é: {num2}");
+    }
+    else if (num3 > num1 && num3 > num2)
+    {
+        System.Console.WriteLine($"O número maior é: {num3}");
+    }
+    else
+    {
+        System.Console.WriteLine("Os números são iguais!");
+    }
+
+}
