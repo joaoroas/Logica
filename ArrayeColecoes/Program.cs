1e7063b80a0b3877b17db1c31621bcbb87328702
Joao Roas <jr.roas@gmail.com>
2022-07-31 16:49:43 -0300

    exercício lista

--- ArrayeColecoes/Program.cs
+++ ArrayeColecoes/Program.cs
@@ -3,24 +3,60 @@
    e uma lista com o nome da fruta e seu respectivo valor.
 */
 
-List<string> frutas = new List<string>();
-List<int> quantidade = new List<int>();
 
-for (var i = 0; i < 10; i++)
+
+// Uma lista dentro da outra, incremento do valor total, formatação duas casas decimais.
+static void ProgramadorFez()
 {
-    System.Console.WriteLine("Digite o nome da fruta:");
-    frutas.Add(Console.ReadLine());
-    System.Console.WriteLine("Digite a o valor:");
-    int a = Convert.ToInt32(Console.ReadLine());
-    quantidade.Insert(i, a);
+List<List<string>> listaFrutas = new List<List<string>>();
+
+for (var i = 0; i < 2; i++)
+{
+    List<string> lista = new List<string>();
+    Console.WriteLine("Digite o nome da fruta:");
+    lista.Add(Console.ReadLine());
+    Console.WriteLine("Digite a o valor:");
+    lista.Add(Console.ReadLine());
+
+    listaFrutas.Add(lista);
+}
+double total = 0;
+foreach (var item in listaFrutas)
+{
+    Console.WriteLine($"Fruta: {item[0]} Valor R$: {item[1]}");
+    Console.WriteLine("==========================");
+
+    total += Convert.ToDouble(item[1]);
+}
+System.Console.WriteLine($"Valor total da compra R$: {total.ToString("0.00")}");
 }
 
 
-for (int i = 0; i < 10; i++)
+
+// Duas listas separadas, Valor total usando Linq, Formatação representando moeda em reais.
+static void ComoEuFiz()
 {
-    System.Console.WriteLine($"Fruta: {frutas[i]} Quantidade: {quantidade[i]}");
+    List<string> frutas = new List<string>();
+    List<string> valor = new List<string>();
+
+    for (var i = 0; i < 2; i++)
+    {
+        Console.WriteLine("Digite o nome da fruta:");
+        frutas.Add(Console.ReadLine());
+        Console.WriteLine("Digite a o valor:");
+        valor.Add(Console.ReadLine());
+    }
+    for (var i = 0; i < 2; i++)
+    {
+        Console.WriteLine($"Fruta: {frutas[i]} Preço R$: {valor[i]}");
+        Console.WriteLine("=================================");
+    }
+
+    decimal total = 0;
+    total = valor.Sum(x => Convert.ToDecimal(x));
+    Console.WriteLine($"O valor total da compra é  {total.ToString("C")}");
 }
 
-System.Console.WriteLine($"A quantidade de frutas no carrinho é: {quantidade.Sum()}");
+
 
 
